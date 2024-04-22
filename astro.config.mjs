6147c8fb0d074f74b4e7f04d35d26843b30d2839
marcoGoldChains <marcos.jimenezfernandez13@gmail.com>
2024-04-22 12:45:59 +0200

    Añadidos los componentes de Vue para generar el efecto de scroll al proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-import { server } from 'typescript';
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-    
-    output:'server',
-
+  output: 'server',
+  integrations: [vue()]
 });
-
