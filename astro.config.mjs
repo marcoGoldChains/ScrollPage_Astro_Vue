4c9e33359d795350a9bd27cdf3cc1aadcc75810f
marcoGoldChains <marcos.jimenezfernandez13@gmail.com>
2024-04-22 11:21:35 +0200

    Proyecto configurado a falta de borrar archivos iniciales

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import { server } from 'typescript';
+
+// https://astro.build/config
+export default defineConfig({
+    
+    output:'server',
+
+});
+
